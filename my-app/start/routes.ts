1973deb684d41d510be58b40f6e2bf34d53ccfb0
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-20 09:43:25 -0300

    feat: added articles routes

---
+++ my-app/start/routes.ts
@@ -0,0 +1,22 @@
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.get('/', async ({ view }) => { return view.render("welcome") })
+
+Route.get("/news", "ArticlesController.view").as("news_view")
+Route.get("/news/create","ArticlesController.create" ).as("news_create")
+Route.post("/news", "ArticlesController.store").as("news_store")
+Route.get("/news/:slug", "ArticlesController.edit").as("news_edit")
+
+Route.patch("/news/:id", ({params}) => {
+  return params
+}).where("id", {
+  match: /^[0-9]+$/,
+  cast: (id) => Number(id),
+})
+
+Route.delete("/news/:id", ({params}) => {
+  return params
+}).where("id", {
+  match: /^[0-9]+$/,
+  cast: (id) => Number(id),
+})
