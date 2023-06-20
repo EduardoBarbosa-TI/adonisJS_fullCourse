502f7f6ac4e87142ae460893fea6dff9c344b350
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-20 13:49:48 -0300

    feat: added methods and renaming

--- my-app/app/Controllers/Http/ArticlesController.ts
+++ my-app/app/Controllers/Http/ArticlesController.ts
@@ -5,22 +5,44 @@ import CreateArticleValidator from "App/Validators/CreateArticleValidator"
 
 export default class ArticlesController {
 
-  public async view({ view }) {
+  public async index({ view }) {
     const articles = await Database.from("articles").select("*")
     return view.render("article.view", { articles })
   }
 
-  public async create({ view }){
+  public create({ view }){
     return view.render("article.create")
+  }
+
+  public async show({view,params}){
+    const article = await Database.from("articles").where("slug", params.slug).first()
+
+    return view.render("article/show",{article})
   }
 
   public async store({request, response}){
     const payload = await request.validate(CreateArticleValidator)
-    await Database.table("articles").insert({ ...payload, slug: 'sgis'})
+    await Database.table("articles").insert({
+        ...payload,
+        slug: payload.title.replace(" ","-") + +new Date()
+      })
     return response.redirect().back()
   }
 
-  public async edit({ view }){
-    return view.render("article/edit")
+  public async edit({ view, params }){
+    const {slug} = params
+    const article = await Database.from("articles").where("slug", slug).first()
+    return view.render("article.edit", { article })
+  }
+
+  public async update({request,response, params}){
+    const payload = await request.validate(CreateArticleValidator)
+    await Database.from("articles").where('slug',params.slug).update(payload)
+    return response.redirect().back()
+  }
+
+  public async destroy({params, response}){
+    await Database.from("articles").where("slug", params.slug).delete()
+    return response.redirect().back()
   }
 }
